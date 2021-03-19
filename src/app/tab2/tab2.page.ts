eb9dc59d1a35d01a3a9a2e2ae54c389d3be5c3b4
c14180100 <62042731+c14180100@users.noreply.github.com>
2021-03-19 10:38:16 +0700

    done

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FotoService } from '../services/foto.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  randomNumber : Number;
+  nomor : Number;
+  showPhoto : Boolean;
+
+  constructor(public fotoService2 : FotoService) {
+    var min = Math.ceil(1);
+    var max = Math.floor(10);
+    
+    this.randomNumber = Math.floor(Math.random() * (max - min) + min);
+    this.showPhoto = false;
+  }
+
+  async ngOnInit(){
+
+  }
+
+  TambahFoto(){
+    this.fotoService2.tambahFoto();
+  }
+
+  guess(){
+    if(this.nomor == this.randomNumber){
+      this.showPhoto = true;
+    }
+  }
 
 }
